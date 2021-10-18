80dd292575044ce2733d49e606c4a6896efae9b2
Leon Pelc <leon.pelc@gmail.com>
2021-10-18 18:48:20 +0200

    ignore blank lines

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ with open("seznam.csv", "r") as seznam:
 
 for row in seznam_rows:
     row_data = row.split(",")
-    print("{0} is {1} years old and {2}.".format(row_data[0],row_data[1],row_data[2]))
+    if len(row_data) == 3:
+        print("{0} is {1} years old and {2}.".format(row_data[0],row_data[1],row_data[2]))
